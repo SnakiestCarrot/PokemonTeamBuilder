650b01b695a8654552669664562a3c28acf19464
AugustWikdahl <augustwikdahl@gmail.com>
2024-11-27 15:26:25 +0100

    modelToPersistence()

--- src/firebaseModel.js
+++ src/firebaseModel.js
@@ -6,13 +6,9 @@ import { firebaseConfig } from "/src/firebaseConfig.js";
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-// Define reference and data
-const REF = "test";
-const rf = ref(database, REF); // Use the initialized database
-
 
 /*TODO
-1. Create firebaseToModel, modelToFirebase, readFromFirebase, saveToFirebase, connectToFirebase
+1. Create modelToPersistence, persistenceToModel, readFromFirebase, saveToFirebase, connectToFirebase
 2. Try with dummy data
 3. Try to implement eparate listeners for each different data
 4. Done for now with empty listener list in the reaction-functionn
@@ -21,9 +17,34 @@ const rf = ref(database, REF); // Use the initialized database
 7. Update security rules in firebase with userspecific rules.
 */
 
-export function modelToPersistence(model){
-    //TODO: Check model data.
-    return {
-        dummyData : 0 
+export function modelToPersistence(model) {
+    // Transform the pokemon object to its id
+    function transformToPokemonId(pokemonObject) {
+        if (pokemonObject && pokemonObject.id) {
+            return pokemonObject.id; 
+        }
+        return null;
     }
+
+    // Transform and sort the team
+    function transformAndSortTeam(team) {
+        // Convert currentTeam object to an array of [key, value] pairs
+        const transformedTeam = Object.entries(team)  // Converts the object into an array of [key, value] pairs
+            .map(function(entry) {  
+                return transformToPokemonId(entry[1]); 
+            })
+            .filter(function(id) {  
+                return id !== null;  
+            })
+            .sort(function(a, b) {  
+                return a - b; 
+            });
+
+        return transformedTeam;
+    }
+
+    return {
+        currentPokemonSearchId: model.currentPokemonId,
+        currentTeamBuilt: transformAndSortTeam(model.currentTeam) // Use transformAndSortTeam for the currentTeam
+    };
 }
