05e807120a8d6eda7688a0fc8ebf24e7a818de66
AugustWikdahl <augustwikdahl@gmail.com>
2024-11-28 12:02:00 +0100

    connected app to firebase

--- src/firebaseModel.js
+++ src/firebaseModel.js
@@ -1,26 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, get, onValue, child } from "firebase/database";
 import { firebaseConfig } from "/src/firebaseConfig.js";
-import { getAuth } from "firebase/auth";
+import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
 
 //Init firebase db
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const db = getDatabase(app);
 
 //Init authorization
 const auth = getAuth(app);
 
-
-
-/*TODO
-1. Create modelToPersistence, persistenceToModel, readFromFirebase, saveToFirebase, connectToFirebase
-2. Try with dummy data
-3. Try to implement eparate listeners for each different data
-4. Done for now with empty listener list in the reaction-functionn
-5. Implement per-user persistence
-6. Implement Google login
-7. Update security rules in firebase with userspecific rules.
-*/
 
 export function modelToPersistence(model) {
     // Transform the pokemon object to its id
@@ -53,5 +42,83 @@ export function modelToPersistence(model) {
         currentTeamBuilt: transformAndSortTeam(model.currentTeam) // Use transformAndSortTeam for the currentTeam
     };
 }
+
+export function persistenceToModel(persistenceData, model) {
+    //TODO peristenceToModel depends on if there is data in 
+    //firebase. Look at dinnerplanner.
+}
+
+export function saveToFirebase(model) {
+    // saveToFirebase: 
+    // -----------------
+    // do nothing if model.user falsy
+    // otherwise write to the same path as above, 
+    // depending on model.ready as usual
+    if (model.user) {
+        if (model.ready) {
+            set(ref(db,"PokemonTeamBuilder/"+model.user.uid), modelToPersistence(model));
+        }
+    } else {
+        //TODO: wipe data??
+    }
+}
+
+export function readFromFirebase(model) {
+// readFromFirebase: 
+// -----------------
+// do nothing if model.user falsy (maybe wipe the model data)
+// otherwise read from "path/"+model.user.uid
+// manage model.ready as 
+    if (model.user) {
+        function persistenceToModelACB(snapshot) {
+            return persistenceToModel(snapshot.val(), model);
+        }
+    
+        function modelReadyACB() {
+            model.ready = true;
+        }
+    
+        model.ready = false;
+        return get(ref(db, "PokemonTeamBuilder/"+model.user.uid)).then(persistenceToModelACB).then(modelReadyACB);
+    } else {
+        //TODO: Wipe data?
+    }
+}
+
+export function connectToFirebase(model, watchFunction) {
+    function checkModelPropertiesACB(){
+        
+    }
+    function saveModelToFirebaseACB(){
+        saveToFirebase(model);
+    }
+  
+    function watchFunctionACB(){
+        watchFunction(checkModelPropertiesACB, saveModelToFirebaseACB);
+    }
+
+    function loginOrOutACB(user) {
+        model.user= user
+        model.ready=false
+
+        if (user) {
+            readFromFirebase(model).then (() => {
+                watchFunctionACB();
+            }).catch((error) => {
+                console.error("Error reading from Firebase:", error);
+            });
+        } else {
+            //TODO: wipe data?? Or set to default?
+        }
+    }
+
+    onAuthStateChanged(auth, loginOrOutACB);
+}
+
+// UI: 
+// - model.user undefined: show suspense because firebase auth not initialized yet
+// - model.user null: shouw "sign in" UI (a button/link that triggers signInWithPopup, see below)
+// - model.user truthy: should the App (based on model.ready!) with the "sign out" UI
+
 
 export { auth };
--- src/reactjs/index.jsx
+++ src/reactjs/index.jsx
@@ -13,3 +13,10 @@ createRoot(document.getElementById('root'))
     <ReactRoot model={reactiveModel}></ReactRoot>
     
 );  
+
+
+//connecting to firebase
+import { connectToFirebase } from "../firebaseModel.js";
+import { reaction } from "mobx";
+configure({ ennforceActions: "never", })
+connectToFirebase(reactiveModel, reaction);
