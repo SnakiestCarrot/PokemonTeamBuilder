d6e1be97ef71cc925a7c61ac356e4bb0bc01cca9
Filip Boive <56317185+bangobongo@users.noreply.github.com>
2024-12-04 13:07:49 +0100

    pokemon inspect done except radar chart

--- src/views/inspectView.jsx
+++ src/views/inspectView.jsx
@@ -13,6 +13,14 @@ export function InspectView(props) {
                         className="pokemon-image"
                     />
                 </div>
+                <div className="pokemon-stats">
+                    <h2>Health: {props.currentPokemon.stats[0].base_stat}</h2>
+                    <h2>Attack: {props.currentPokemon.stats[1].base_stat}</h2>
+                    <h2>Defense: {props.currentPokemon.stats[2].base_stat}</h2>
+                    <h2>Special Attack: {props.currentPokemon.stats[3].base_stat}</h2>
+                    <h2>Special Defense: {props.currentPokemon.stats[4].base_stat}</h2>
+                    <h2>Speed: {props.currentPokemon.stats[5].base_stat}</h2>
+                </div>
             </div>
         </div>
     )
