41df1b66f5238b71e5321eac000c1617eea6b194
Casper Johansson <caspthepro@gmail.com>
2024-12-04 14:42:31 +0100

    Added skeleton code for teamView

--- src/presenters/teamBuilderPresenter.jsx
+++ src/presenters/teamBuilderPresenter.jsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
 import { PokemonResultView } from "../views/PokemonResultView";
 import { PokemonSearchView } from "../views/PokemonSearchView";
+import { TeamBuilderView } from "../views/TeamBuilderView"
 import { useEffect } from "react";
 
 const TeamBuilder = observer(
@@ -29,6 +30,9 @@ const TeamBuilder = observer(
                 <PokemonSearchView
                     updatePokemonSearchACB={handleSearchInput}
                     text={props.model.searchQuery}
+                />
+                <TeamBuilderView 
+                    
                 />
                 <PokemonResultView 
                     pokemonResults={props.model.filteredPokemon}
---
+++ src/views/TeamBuilderView.jsx
@@ -0,0 +1,20 @@
+
+
+// This is the view that will display the current team when making edits to it
+export function TeamBuilderView(props) {
+
+    return (
+        <div>
+            <div>
+                <p>This will be the team builder view</p>
+            </div>
+        </div>
+    );
+}
+
+
+
+
+
+
+
