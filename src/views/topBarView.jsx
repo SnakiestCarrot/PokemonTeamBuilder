7eea227e360348d5040ec9ee5227ae2068d8c953
AugustWikdahl <augustwikdahl@gmail.com>
2024-12-05 10:50:21 +0100

    login/logout button on topbar presenter

--- src/views/topBarView.jsx
+++ src/views/topBarView.jsx
@@ -1,11 +1,12 @@
-export function TopBarView() {
+export function TopBarView(props) {
     return (
-    <div className="topBar">
-        <a href="#main">Menu</a>
-        <a href="#teambuilder">Team Builder</a>
-        <a href="#todo">Leaderboard</a>
-        <a href="#teams">Teams</a>
-        <a href="#login">Login</a>
-    </div>
-    )
+        <div className="topBar">
+            <a href="#main">Menu</a>
+            <a href="#teambuilder">Team Builder</a>
+            <a href="#todo">Leaderboard</a>
+            <a href="#teams">Teams</a>
+            <a onClick={props.onLoginClick}>Login</a>
+            <a onClick={props.onLogoutClick}>Logout</a>
+        </div>
+    );
 }
