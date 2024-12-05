4b35837172cfdb974af33c358fe02b5b36f81c53
Alexander Larsson <alexala@kth.se>
2024-12-05 11:41:53 +0100

    Router for teams page

--- src/reactjs/ReactRoot.jsx
+++ src/reactjs/ReactRoot.jsx
@@ -5,6 +5,7 @@ import { Login } from "../presenters/loginPresenter";
 import { TeamBuilder } from "../presenters/teamBuilderPresenter";
 import { Inspect } from "../presenters/inspectPresenter";
 import { TopBarPresenter } from "../presenters/topBarPresenter";
+import { MyTeams } from "../presenters/myTeamsPresenter";
 
 const ReactRoot = observer(
     function ReactRoot(props){
@@ -44,7 +45,12 @@ function makeRouter (model) {
         {
             path:"inspect",
             element: <Inspect model={model}></Inspect>
+        },
+        {
+            path:"teams",
+            element: <MyTeams model={model}></MyTeams>
         }
+
     ]
 
     return (createHashRouter(routerArray))
