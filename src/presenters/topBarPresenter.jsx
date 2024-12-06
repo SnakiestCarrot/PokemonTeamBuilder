23d4ad155f353646e7be29f621afc4381a79f5fd
AugustWikdahl <augustwikdahl@gmail.com>
2024-12-06 12:05:56 +0100

    removed redundant console.log

--- src/presenters/topBarPresenter.jsx
+++ src/presenters/topBarPresenter.jsx
@@ -2,28 +2,21 @@ import { observer } from "mobx-react-lite";
 import { TopBarView } from "../views/topBarView";
 
 const TopBar = observer(function TopBarRender(props) {
-    console.log("TopBar re-rendered. User state:", props.model.user);
 
     function userWantsToLogin() {
-        console.log("Before login, user:", props.model.user);
         props.model.userWantsToLogin();
-        console.log("After login, user:", props.model.user);
     }
 
     function userWantsToLogout() {
-        console.log("Before logout, user:", props.model.user);
         props.model.userWantsToLogout();
-        console.log("After logout, user:", props.model.user);
     }
 
     function userClicksProfile(event) {
         event.stopPropagation(); // Prevent click from propagating to the document
-        console.log("Profile clicked. Dropdown visible:", props.model.isDropdownVisible);
         props.model.toggleDropDown();
     }
 
     function closeDropdown() {
-        console.log("Dropdown closed.");
         props.model.closeDropDown();
     }
 
